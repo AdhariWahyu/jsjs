64ccf28ae93c00e4715dbb58c19ce102973ae6b4
AdhariWahyu <wahyuwidito24@gmail.com>
2022-07-17 10:39:23 +0700

    track js

---
+++ conditional.js
@@ -0,0 +1,11 @@
+// CONDITIONAL
+
+var number = 10;
+
+if(number === 10){
+    console.log("Angka 10")
+} else if (number === 12){
+    console.log("Angka 12")
+}else{
+    console.log("Angka bukan 10 dan 12")
+}
